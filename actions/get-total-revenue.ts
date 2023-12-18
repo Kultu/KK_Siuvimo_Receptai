a7e3b0eb00845296088695227db03d9117a83cbd
Kultu <vilhelmaskacinskas@gmail.com>
2023-12-18 16:41:38 +0200

    test

--- actions/get-total-revenue.ts
+++ actions/get-total-revenue.ts
@@ -1,20 +1,6 @@
 import prismadb from "@/lib/prismadb";
 
 export const getTotalRevenue = async (storeId: string) => {
-  const test = await prismadb.order.findMany({
-    where: {
-      storeId,
-      isPaid: true
-    },
-    include: {
-      orderItems: {
-        include: {
-          product: true
-        }
-      }
-    }
-  });
-
   const paidOrders = await prismadb.store.findMany({
     where: {
       id : storeId
